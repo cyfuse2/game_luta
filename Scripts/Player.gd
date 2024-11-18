08036c38bc214388560cccd902521b8025e9e2b3
Carlos Somenzari Goltzman <149161450+cyfuse2@users.noreply.github.com>
2024-11-18 09:22:35 -0300

    Add files via upload

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,21 +1,21 @@
 extends CharacterBody2D 
 
 @export var speed = 130
-@export var jump_velocity = -500 
+@export var jump_velocity = -300.0
 @export var max_health: int
 @export var health = 0 : set = _set_health
-
+@export var ataque = 5
 @onready var bar_1 = $ProgressBar
-@onready var timer = $Timer
+@onready var timer 
 @onready var timer_attack: Timer = $Timer_Attack
 @onready var delay_attack: Timer = $Delay_Attack
 
 var dir 
-var gravity = 6000 
-var jumps = 1
+var gravity = 6000
 var pode_atacar = true
 var attack_index = 0
-
+var is_attacking = false
+var life = 2
 
 func _set_health(new_health):
 	if new_health <= 0:
@@ -35,7 +35,7 @@ func _set_health(new_health):
 	pass
 	
 
-@onready var animator = $AnimSprite 
+@onready var animator = $AnimSprite
 
 func _physics_process(delta):
 	Move(delta)
@@ -47,47 +47,119 @@ func _physics_process(delta):
 func Move(delta): 
 	dir = Input.get_axis("Esquerda" , "Direita") 
 	
+	
 	if dir: 
 		velocity.x = dir * speed 
 	else: 
 		velocity.x = 0  
 		
 	if not is_on_floor(): 
-		velocity.y =+ gravity * delta 
+		velocity.y += gravity * delta 
 		
 	if Input.is_action_just_pressed("Pulo") and is_on_floor(): 
-		velocity.y = jump_velocity
+		velocity.y += jump_velocity
 		
 	move_and_slide()
 	pass 
 
 
 func Animations():
-	if velocity.x != 0 and is_on_floor(): 
-		animator.play("walk")
-		
-	elif velocity.x == 0 and is_on_floor(): 
-		animator.play('idle')
-		
+	if pode_atacar:
+		if velocity.x != 0 and is_on_floor(): 
+			animator.play("walk")
+		elif velocity.x == 0 and is_on_floor(): 
+			animator.play("idle")
 	if not is_on_floor(): 
-		animator.play('jump')
+		if pode_atacar:
+			animator.play("jump")
 	pass
 	
 func Attack():
-	if pode_atacar:
-		timer_attack.start()
-		attack_index += 1
-	if attack_index >= 3:
+	if pode_atacar and Input.is_action_just_pressed("Ataque1"):
+		if animator.animation != "attack1":
+		# Inicia a animação de ataque
+			animator.play("attack1")
+		# Controla a lógica de ataque (tempo entre ataques)
 		pode_atacar = false
-		delay_attack.start()
+		$Timer_Attack.wait_time = 0.8
+		timer_attack.start()  # Inicia o temporizador do ataque
+		
+		attack_index += 1
+		print(attack_index)
+		$HitBox/Hit1.disabled = false
+		if attack_index >= 3:
+			# Se o personagem atingir o número máximo de ataques consecutivos, aguarda antes de poder atacar novamente
+			delay_attack.start()
+			
+	if pode_atacar and Input.is_action_just_pressed("Ataque2"):
+		if animator.animation != "attack2":
+		# Inicia a animação de ataque
+			animator.play("attack2")
+		# Controla a lógica de ataque (tempo entre ataques)
+		pode_atacar = false
+		$Timer_Attack.wait_time = 0.8
+		timer_attack.start()  # Inicia o temporizador do ataque
+		
+		attack_index += 1
+		print(attack_index)
+		$HitBox/Hit2.disabled = false
+		if attack_index >= 3:
+			delay_attack.start()
+			
+	if pode_atacar and Input.is_action_just_pressed("Ataque3"):
+		if animator.animation != "attack3":
+			animator.play("attack3")
+		pode_atacar = false
+		$Timer_Attack.wait_time = 0.8
+		timer_attack.start()
+		
+		attack_index += 1
+		print(attack_index)
+		$HitBox/Hit3.disabled = false
+		if attack_index >= 3:
+			delay_attack.start()
+			
+	pass
+	
+func  Hit():
+	animator.play("Attack1")
+	life -= 5
+	await get_tree().create_timer(0.4).timeout
+	animator.play("idle")
 	pass
 	
 
 func _on_timer_attack_timeout():
-	attack_index = 0
+	pode_atacar = true
+	animator.play("idle")
+	$HitBox/Hit1.disabled = true
+	$HitBox/Hit2.disabled = true 
+	$HitBox/Hit3.disabled = true
 	pass
 
 
 func _on_delay_attack_timeout():
+	attack_index = 0
 	pode_atacar = true
 	pass
+
+
+func _on_hit_box_body_entered(body):
+	if body.is_in_group("Players"):
+		body.Hit()
+	pass # Replace with function body.
+
+#
+#func _on_anim_sprite_animation_finished(anim_name):
+	#if anim_name == "attack1":
+		## A animação terminou, volta para idle
+		#animator.play("idle")
+		## Desmarca o ataque
+		#is_attacking = false
+		#
+	#if anim_name == "attack2":
+		## A animação terminou, volta para idle
+		#animator.play("idle")
+		## Desmarca o ataque
+		#is_attacking = false
+	#pass
--- Scripts/player_2.gd
+++ Scripts/player_2.gd
@@ -1,61 +1,161 @@
 extends CharacterBody2D 
 
 @export var speed = 130
-@export var jump_velocity = -500 
+@export var jump_velocity = -1000.0
+@export var max_health: int
+@export var health = 0 : set = _set_health
+@export var ataque = 5
+@onready var bar_1 = $ProgressBar
+@onready var timer 
+@onready var timer_attack: Timer = $Timer_Attack
+@onready var delay_attack: Timer = $Delay_Attack
 
 var dir 
 var gravity = 6000
-var jumps = 1
+var pode_bater = true
+var attack_index = 0
+var is_attacking = false
 
-@onready var animator = $AnimSprite 
 
-func _physics_process(delta): 
+func _set_health(new_health):
+	if new_health <= 0:
+		queue_free()
+		return
+	if new_health >= max_health:
+		new_health = max_health
+		
+	if new_health >= health:
+		health = new_health
+		bar_1.value = health
+	else:
+		health = new_health
+		
+		bar_1.value = health
+		timer.start(0.5)
+	pass
 	
+
+@onready var animator = $AnimSprite
+
+func _physics_process(delta):
 	Move(delta)
 	Animations()
+	Attack()
 	pass 
 	
-func Move(delta): 
 	
+func Move(delta): 
 	dir = Input.get_axis("Left" , "Right") 
+	
 	
 	if dir: 
 		velocity.x = dir * speed 
-		
 	else: 
 		velocity.x = 0  
 		
 	if not is_on_floor(): 
-		velocity.y =+ gravity * delta 
+		velocity.y += gravity * delta 
 		
 	if Input.is_action_just_pressed("Jump") and is_on_floor(): 
-		velocity.y = jump_velocity
+		velocity.y += jump_velocity
 		
 	move_and_slide()
 	pass 
 
 
 func Animations():
-	
-	if velocity.x != 0 and is_on_floor(): 
-		animator.play("walk") 
-		
-	elif velocity.x == 0 and is_on_floor(): 
-		animator.play('idle') 
-		
-		
+	if pode_bater:
+		if velocity.x != 0 and is_on_floor(): 
+			animator.play("walk")
+		elif velocity.x == 0 and is_on_floor(): 
+			animator.play("idle")
 	if not is_on_floor(): 
-		animator.play('jump') 
+		if pode_bater:
+			animator.play("jump") 
+	#if velocity.x != 0 and is_on_floor(): 
+		#animator.play("run")   
+		
+	pass
+	
+func Attack():
+	if pode_bater and Input.is_action_just_pressed("Attack1"):
+		if animator.animation != "hit1":
+		# Inicia a animação de ataque
+			animator.play("hit1")
+		# Controla a lógica de ataque (tempo entre ataques)
+		pode_bater = false
+		$Timer_Attack.wait_time = 0.6
+		timer_attack.start()  # Inicia o temporizador do ataque
+		
+		attack_index += 1
+		print(attack_index)
+		$HitBox/Hit1.disabled = false
+		if attack_index >= 3:
+			# Se o personagem atingir o número máximo de ataques consecutivos, aguarda antes de poder atacar novamente
+			delay_attack.start()
+			
+	if pode_bater and Input.is_action_just_pressed("Attack2"):
+		if animator.animation != "hit2":
+		# Inicia a animação de ataque
+			animator.play("hit2")
+		# Controla a lógica de ataque (tempo entre ataques)
+		pode_bater = false
+		$Timer_Attack.wait_time = 0.4
+		timer_attack.start()  # Inicia o temporizador do ataque
+		
+		attack_index += 1
+		print(attack_index)
+		$HitBox/Hit2.disabled = false
+		if attack_index >= 3:
+			delay_attack.start()
+			
+	if pode_bater and Input.is_action_just_pressed("Attack3"):
+		if animator.animation != "hit3":
+			animator.play("hit3")
+		pode_bater = false
+		$Timer_Attack.wait_time = 0.8
+		timer_attack.start()
+		
+		attack_index += 1
+		print(attack_index)
+		$HitBox/Hit3.disabled = false
+		if attack_index >= 3:
+			delay_attack.start()
+			
+	pass
+	
+
+func _on_timer_attack_timeout():
+	pode_bater = true
+	animator.play("idle")
+	$HitBox/Hit1.disabled = true
+	$HitBox/Hit2.disabled = true 
+	$HitBox/Hit3.disabled = true
 	pass
 
-func Attack():
-	if Input.is_action_just_pressed("Hit1"):
-		animator.play('attack1')
-		
-	if Input.is_action_just_pressed("Hit2"):
-		animator.play('attack2')
-		
-	if Input.is_action_just_pressed("Hit3"):
-		animator.play('attack3')
-		
+
+func _on_delay_attack_timeout():
+	attack_index = 0
+	pode_bater = true
 	pass
+
+
+func _on_hit_box_body_entered(body):
+	if body.is_in_group("Players"):
+		body.Hit()
+	pass # Replace with function body.
+
+#
+#func _on_anim_sprite_animation_finished(anim_name):
+	#if anim_name == "attack1":
+		## A animação terminou, volta para idle
+		#animator.play("idle")
+		## Desmarca o ataque
+		#is_attacking = false
+		#
+	#if anim_name == "attack2":
+		## A animação terminou, volta para idle
+		#animator.play("idle")
+		## Desmarca o ataque
+		#is_attacking = false
+	#pass
